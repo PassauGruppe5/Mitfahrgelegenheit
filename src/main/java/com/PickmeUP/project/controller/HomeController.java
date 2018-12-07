adc26de1db1f99185a02bb557b8b7556907d66b7
Tobias Herrmann <Kabaisback@gmail.com>
2018-12-08 00:55:53 +0100

    created registrationController, registration.html

--- src/main/java/com/PickmeUP/project/controller/HomeController.java
+++ src/main/java/com/PickmeUP/project/controller/HomeController.java
@@ -1,25 +1,26 @@
 package com.PickmeUP.project.controller;
 
-import com.PickmeUP.project.model.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.time.LocalDateTime;
 
 @Controller
-public class HomeController {
+class HomeController {
 
     @GetMapping("/")
-    public String userForm(Model model) {
-        model.addAttribute("user", new User());
+    String index(Model model) {
+        model.addAttribute("now", LocalDateTime.now());
+
         return "index";
     }
 
-    @PostMapping("/")
-    public String userSubmit(@ModelAttribute User user) {
-
-        return "result";
+    @GetMapping("properties")
+    @ResponseBody
+    java.util.Properties properties() {
+        return System.getProperties();
     }
 
 }
