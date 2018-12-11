ae55cf1dd95bcd4c938732b6e94dece0695573cc
Tobias Herrmann <Kabaisback@gmail.com>
2018-12-11 12:35:01 +0100

    Connected Database to Insert Values created by the form in registration.xml. Also added all Columns in the User Entity combined with their respective getters and setters.

--- src/main/java/com/PickmeUP/project/controller/RegistrationController.java
+++ src/main/java/com/PickmeUP/project/controller/RegistrationController.java
@@ -1,24 +1,36 @@
 package com.PickmeUP.project.controller;
 
 import com.PickmeUP.project.model.User;
+import com.PickmeUP.project.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.validation.Valid;
 
 @Controller
 public class RegistrationController {
 
-    @GetMapping("/registration")
-    public String userForm(Model model) {
-        model.addAttribute("user", new User());
-        return "registration";
+    @Autowired
+
+    private UserRepository userRepository;
+
+    @RequestMapping(value = "/registration", method = RequestMethod.GET)
+    public ModelAndView showForm() {
+        return new ModelAndView( "registration", "user", new User());
     }
 
-    @PostMapping("/registration")
-    public String userSubmit(@ModelAttribute User user) {
-
+    @RequestMapping(value = "/registration", method = RequestMethod.POST)
+    public String userSubmit(@Valid @ModelAttribute ("user") User user , BindingResult result, ModelMap model) {
+        if(result.hasFieldErrors()) {
+            return "error";
+        }
+        userRepository.save(user);
+        model.addAttribute("first_name", user.getFirst_name());
+        model.addAttribute ("last_name", user.getLast_name());
         return "result";
     }
 
