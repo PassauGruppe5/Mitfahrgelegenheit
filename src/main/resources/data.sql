69966de95b49e8dbfe7ca8468e305bf4545bf09e
Tobias Herrmann <Kabaisback@gmail.com>
2019-01-24 22:06:05 +0100

    Added new Bonusprogramms.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,9 @@
 REPLACE INTO `role` VALUES (1,'USER');
 REPLACE INTO `role` VALUES (2,'ADMIN');
 REPLACE INTO `bonus` VALUES (1,'Kein Bonusprogramm');
+REPLACE INTO `bonus` VALUES (2,'Heavy User');
+REPLACE INTO `bonus` VALUES (3,'10% Rabatt auf nächste Fahrt.');
+REPLACE INTO `bonus` VALUES (4,'Nächste Fahrt umsonst.');
 REPLACE INTO `repeats` VALUES (1,'Einmalig');
 REPLACE INTO `repeats` VALUES (2,'Täglich');
 REPLACE INTO `repeats` VALUES (3,'Wöchentlich');
