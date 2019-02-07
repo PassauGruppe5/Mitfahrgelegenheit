6c155d44b579f11fd130dea9a539df11d235f059
Tobias Herrmann <Kabaisback@gmail.com>
2019-02-07 18:27:01 +0100

    Added comments

--- src/main/java/com/PickmeUP/project/SchedulerTasks.java
+++ src/main/java/com/PickmeUP/project/SchedulerTasks.java
@@ -34,12 +34,22 @@ public class SchedulerTasks {
 
     private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
 
+    //      every 30 minutes journeys with an arrival date and later than the current values
+    //      will be set to inactive (active = 0)
+    //
+    //      @return void
     @Scheduled(cron = "0 */30 * * * *")
     public void setDoneJourneysToInactive(){
+        //log the running of the service.
         log.info("Deactivated ended journeys.", dateFormat.format(new Date()));
+
+        //get all done journeys.
         ArrayList<Journey> doneJourneys = journeyService.findAllByActiveAndCanceled(1,0);
+
+        //get current date and time values.
         LocalDateTime current = LocalDateTime.now();
 
+        //mark a jounrey as inactive if arrival date and time is later than the current values.
         for(Journey done : doneJourneys ) {
                 if (current.isAfter(LocalDateTime.of(LocalDate.parse(done.getArrivalDate()), LocalTime.parse(done.getArrivalTime())))) {
                 done.setActive(0);
@@ -49,22 +59,39 @@ public class SchedulerTasks {
 
     }
 
+    //      every 5 minutes drivers and passengers get a reminder email if their
+    //     booked or offered journey will start in one hour from now.
+    //
+    //      @return void
     @Scheduled(cron = "0 */5 * * * *")
     public void sendReminders(){
+
+        //log running of the service.
         log.info("Reminders sent.", dateFormat.format(new Date()));
+
+        //get all active journeys.
         ArrayList<Journey> journeysToCheck = journeyService.findAllByActiveAndCanceled(1,0);
+
+        //get current values for date and time.
         LocalDateTime current = LocalDateTime.now();
 
+        //check all active journeys for date and time.
         for(Journey done : journeysToCheck ) {
+
+            //if a journey is within 1 hour and 1 hour + 5 mins, a riminder will be sent to driver and
+            //passengers
             if ((current.plusHours(1).isBefore(LocalDateTime.of(LocalDate.parse(done.getDepartureDate()), LocalTime.parse(done.getDepartureTime()))) && current.plusHours(1).plusMinutes(5).isAfter(LocalDateTime.of(LocalDate.parse(done.getDepartureDate()), LocalTime.parse(done.getDepartureTime())))) || current.plusHours(1).isEqual(LocalDateTime.of(LocalDate.parse(done.getDepartureDate()), LocalTime.parse(done.getDepartureTime())))) {
 
+                //get all legs of journey.
                 ArrayList<Leg> legsOfJourneys = legService.findByJourney(done);
 
+                    //send reminder to driver once.
                     for(Leg leg : legsOfJourneys) {
                     List<User> usersToBeReminded = leg.getPassengers();
                     usersToBeReminded.add(done.getDriver());
                     List<User> remindedUsers = new ArrayList<>();
 
+                    //send reminder to every passenger once.
                     for(User user : usersToBeReminded) {
                         if(remindedUsers.contains(user) != true) {
                             remindedUsers.add(user);
